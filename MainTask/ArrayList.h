0be3adb297c5fda9fd6e216e991edd6cd89f1fac
danila-sosnov <dsosnov2009@gmail.com>
2025-05-23 23:58:46 +0300

    Add ArrayList class

---
+++ MainTask/ArrayList.h
@@ -0,0 +1,33 @@
+#include "main.h"
+class ArrayList
+{
+
+private:
+
+	int* list;
+	int size;
+
+
+public:
+
+	ArrayList();
+	~ArrayList();
+
+	void add(int element);
+	void add(int index, int element);
+	void addAll(int* elements, int size);
+	void revome();
+	void remove(int index);
+	void clear();
+	bool isEmpty();
+	int get(int index);
+	void set(int index, int element);
+	int getSize();
+	string toString();
+
+	};
+
+
+
+
+
